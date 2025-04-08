fd35db9f13cad9ff178383348dab9834644e96c1
CJU-PC <prue0115@gmail.com>
2025-04-08 10:57:36 +0900

    2025-04-08

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int kor = 0;
+        int eng = 40;
+        int mat = 90;
+        int sci = 85;
+
+        int sum = kor + eng + mat + sci;
+        int intAvg = sum / 4;
+        double doubleAvg = sum / 4.0;
+
+        System.out.println("합계: " + sum);
+        System.out.println("정수 평균: " + intAvg);
+        System.out.println("실수 평균: " + doubleAvg);
+    }
+}
